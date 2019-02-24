0e076f611f33492ea23ce6eaf611a0d5ac4874f7
psl <pawel.slowik.dev@gmail.com>
2019-02-24 14:22:12 +0100

    Replaced list with table and added some icons and colors

--- src/app/shared/models/todo.ts
+++ src/app/shared/models/todo.ts
@@ -1,10 +1,13 @@
 export class Todo {
-    constructor(public id: number, public title: string, public description: string, public endDate: Date) {
+    constructor(public id: number, public title: string, public description: string,
+                public endDate: Date, public status: string = 'not started') {
     }
 }
 
 export const TODO_LIST: Todo[] = [
-    new Todo(1, 'Learn Angular', 'Read the manual and prepare a demo project', new Date('2019-04-01')),
-    new Todo(2, 'Buy potatoes', 'Pick the best potatoes available!', new Date()),
-    new Todo(3, 'Fix the car', 'Go to local car service', new Date('2019-03-01'))
+    new Todo(1, 'Learn Angular', 'Read the manual and prepare a demo project', new Date('2019-04-01'), 'started'),
+    new Todo(2, 'Buy potatoes', 'Pick the best potatoes available!', new Date(), 'canceled'),
+    new Todo(3, 'Fix the car', 'Go to local car service', new Date('2019-03-01')),
+    new Todo(3, 'Eat breakfast', 'Maybe some corn flakes?', new Date(), 'completed'),
+    new Todo(3, 'Order pizza', 'Large pepperoni', new Date(), 'completed')
   ];
